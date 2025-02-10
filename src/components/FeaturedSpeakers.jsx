220660578179f329b1221a5f52e157ac4b02c3a5
Hassana <hassanaabdll1@gmail.com>
2025-02-10 14:15:07 +0100

    fix-a

--- src/components/FeaturedSpeakers.jsx
+++ src/components/FeaturedSpeakers.jsx
@@ -8,7 +8,7 @@ function FeaturedSpeakers() {
 
   return (
     <section className="py-10 pb-20 bg-gradient-to-br from-primary-light via-white to-secondary-light">
-      <div className="container mx-auto px-6">
+      <div className="container mx-auto ">
         <motion.h2
           initial={{ opacity: 0, y: 20 }}
           whileInView={{ opacity: 1, y: 0 }}
@@ -22,7 +22,7 @@ function FeaturedSpeakers() {
           Meet some of the incredible minds shaping the future of technology.
         </p>
 
-        <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 gap-8 max-w-6xl mx-auto mb-10">
+        <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 gap-4 mx-auto mb-10">
           {speakers.slice(0,3).map((speaker) => (
             <motion.div
               key={speaker.id}
@@ -37,15 +37,15 @@ function FeaturedSpeakers() {
                 {/* Front of card */}
                 <div className="absolute w-full backface-hidden">
                   <div className="bg-white rounded-xl shadow-lg overflow-hidden h-full">
-                    <div className="aspect-w-16 aspect-h-9">
+                    <div className="aspect-w-16 aspect-h-fit-content">
                       <img
                         src={speaker.image}
                         alt={speaker.name}
                         className="w-full h-64 object-cover"
                       />
                     </div>
-                    <div className="p-6">
-                      <h3 className="text-md font-semibold text-navy mb-2">
+                    <div className="p-3">
+                      <h3 className="text-lg font-semibold text-navy mb-2">
                         {speaker.name}
                       </h3>
                       <p className="text-primary-dark mb-2 font-medium">
